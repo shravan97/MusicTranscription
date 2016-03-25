4e0d791db788cef4d3ec3f970cbc0bf4d61b81c7
shravan97 <shravanmurali@gmail.com>
2016-03-26 02:48:02 +0530

    added while loop to get correct input number

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import scipy.io.wavfile as scw
 import numpy as np
 
+num=0
 
-instrument = raw_input("Please enter in the instrument number from the following menu: ")
+while num>=1 and num<=2:
+	num = raw_input("Please enter in the instrument number from the following menu: ")
+	if num>2 or num<1 :
+		print "Please Enter in a valid option"
+
 path = raw_input("Please enter the correct path to the '.wav' file of your music track : ")
 
 music_data = scw.read(path)
